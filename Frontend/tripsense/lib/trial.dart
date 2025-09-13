0f98d010bd25b9411341e07751226042daf3782d
Ayush Musale <aayushmusale05@gmail.com>
2025-09-13 12:47:20 +0530

    trial

---
+++ Frontend/tripsense/lib/trial.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TrialPage extends StatelessWidget {
+  const TrialPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("main")
+          ],
+        ),
+      ),
+    );
+  }
+}
